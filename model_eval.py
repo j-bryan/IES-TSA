fixes ACF error calculation

model_eval.py
@@ -35,20 +35,18 @@ class ModelAssessment:
             # Calculate autocorrelation function for historical and synthetic data
             acf_h, acf_h_confint = acf(vals_h, nlags=24, alpha=0.05)
             acf_s, acf_s_confint = acf(vals_s, nlags=24, alpha=0.05)
-            # Set any ACF values within the 95% CI to 0
-            acf_h[abs(acf_h) < acf_h_confint[:, 1]] = 0
-            acf_s[abs(acf_s) < acf_s_confint[:, 1]] = 0
-            # Calculate the squared error between the historical and synthetic data ACFs, scaling the ACFs by
-            # the CI of the historical data. The CI width increases with lag, thus emphasizing the earlier lag
-            # values in this sum.
-            acf_se = np.sum((acf_h - acf_s) ** 2 / acf_h_confint[:, 1])
+            # If 0 is contained in the 95% CI, set that lag to 0
+            acf_h[acf_h_confint.prod(axis=1) < 0] = 0
+            acf_s[acf_s_confint.prod(axis=1) < 0] = 0
+            # Calculate the squared error between the historical and synthetic data ACFs.
+            acf_se = np.sum((acf_h - acf_s) ** 2)
 
             # Repeat for PACF
             pacf_h, pacf_h_confint = pacf(vals_h, nlags=24, alpha=0.05)
             pacf_s, pacf_s_confint = pacf(vals_s, nlags=24, alpha=0.05)
-            pacf_h[abs(pacf_h) < pacf_h_confint[:, 1]] = 0
-            pacf_s[abs(pacf_s) < pacf_s_confint[:, 1]] = 0
-            pacf_se = np.sum((pacf_h - pacf_s) ** 2 / pacf_h_confint[:, 1])
+            pacf_h[pacf_h_confint.prod(axis=1) < 0] = 0
+            pacf_s[pacf_s_confint.prod(axis=1) < 0] = 0
+            pacf_se = np.sum((pacf_h - pacf_s) ** 2)
 
             data.append([n, ks_res.statistic, ks_res.pvalue, wd, acf_se, pacf_se])
 